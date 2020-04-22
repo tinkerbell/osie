4343272561e131e6d5b944b67f8e47062bd62a48
Dustin Miller <dustin@packet.com>
2020-04-22 10:26:38 -0700

    Bypass kexec for t1.small and Ubuntu 20
    These installs hang at the kexec step, lets not use that method here and
    suffer the delayed provisioning time instead.

--- docker/scripts/osie.sh
+++ docker/scripts/osie.sh
@@ -539,6 +539,7 @@ echo -e "${BYELLOW}Install time: $((etimer - stimer))${NC}"
 case ${OS}:${class} in
 centos_8:t1.small.x86) reboot=true ;;
 ubuntu_18_04:t1.small.x86) reboot=true ;;
+ubuntu_20_04:t1.small.x86) reboot=true ;;
 ubuntu_16_04:c3.medium.x86) reboot=true ;;
 ubuntu_16_04:t3.small.x86) reboot=true ;;
 *:c2.medium.x86) reboot=true ;;
