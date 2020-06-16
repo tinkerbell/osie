cbb6706b7c74ec3acc4b98a18cb30782aed15a42
Manuel Mendez <manny@packet.com>
2020-06-16 10:34:04 -0400

    Update nixpkgs pin

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@ let _pkgs = import <nixpkgs> { };
 in { pkgs ? import (_pkgs.fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs-channels";
-  # nixos-unstable @2019-05-28
-  rev = "eccb90a2d997d65dc514253b441e515d8e0241c3";
-  sha256 = "0ffa84mp1fgmnqx2vn43q9pypm3ip9y67dkhigsj598d8k1chzzw";
+  #branch@date: nixpkgs-unstable@2020-04-23
+  rev = "b95699970fb7d825fd4a710f5cfa3785a98435db";
+  sha256 = "0s9pjym3wi3ssp33cd2sj8fs9dlny5yhc7lhnj2lzadx8ianbf72";
 }) { } }:
 
 with pkgs;
