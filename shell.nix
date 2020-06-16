29da85a3c795839168b8a4de08e5f587f68b9d31
Manuel Mendez <manny@packet.com>
2020-06-16 10:33:29 -0400

    Run nixfmt shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,11 @@
-let
-  _pkgs = import <nixpkgs> {};
-in
-{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
-                                         repo = "nixpkgs-channels";
-                                         # nixos-unstable @2019-05-28
-                                         rev = "eccb90a2d997d65dc514253b441e515d8e0241c3";
-                                         sha256 = "0ffa84mp1fgmnqx2vn43q9pypm3ip9y67dkhigsj598d8k1chzzw";
-                                       }) {}
-}:
+let _pkgs = import <nixpkgs> { };
+in { pkgs ? import (_pkgs.fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs-channels";
+  # nixos-unstable @2019-05-28
+  rev = "eccb90a2d997d65dc514253b441e515d8e0241c3";
+  sha256 = "0ffa84mp1fgmnqx2vn43q9pypm3ip9y67dkhigsj598d8k1chzzw";
+}) { } }:
 
 with pkgs;
 
