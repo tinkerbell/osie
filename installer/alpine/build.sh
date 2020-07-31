3337c5c0436204e2946058bfc0b1cbc40f1fa96f
Manuel Mendez <manny@packet.com>
2020-07-31 11:55:21 -0400

    installer/alpine: Split build.sh into muliptle functions
    This will be necessary in a later commit. This way Make will have 3
    targets, one for each instead of 1 recipe that builds the artifacts.
    Having it in 3 recipes allows the Makefile and make to be trivially
    correct, otherwise we have to bend over backwards to handle rules that
    generate multiple targets and will likely still have some edge cases
    left over. See https://www.gnu.org/software/automake/manual/html_node/Multiple-Outputs.html
    
    Signed-off-by: Manuel Mendez <manny@packet.com>

--- installer/alpine/build.sh
+++ installer/alpine/build.sh
@@ -5,38 +5,50 @@ set -o errexit -o nounset -o pipefail -o xtrace
 # shellcheck disable=SC2039
 [[ $(uname -m) == aarch64 ]] && echo "aarch64 isn't _really_ tested/supported yet" && exit 1
 
-# Eclypsium driver and supporting modules (IPMI)
-cat >/etc/mkinitfs/features.d/eclypsium.modules <<EOF
-extra/eclypsiumdriver.ko
-kernel/drivers/char/ipmi/*.ko
-EOF
+build_initramfs() {
+	# Eclypsium driver and supporting modules (IPMI)
+	cat >/etc/mkinitfs/features.d/eclypsium.modules <<-EOF
+		extra/eclypsiumdriver.ko
+		kernel/drivers/char/ipmi/*.ko
+	EOF
 
-cat >/etc/mkinitfs/features.d/network.modules <<EOF
-kernel/drivers/net/ethernet
-kernel/net/packet/af_packet.ko
-EOF
+	cat >/etc/mkinitfs/features.d/network.modules <<-EOF
+		kernel/drivers/net/ethernet
+		kernel/net/packet/af_packet.ko
+	EOF
 
-cat >/etc/mkinitfs/features.d/packetrepo.files <<EOF
-/etc/apk/cache/*
-EOF
+	cat >/etc/mkinitfs/features.d/packetrepo.files <<-EOF
+		/etc/apk/cache/*
+	EOF
 
-cat <<EOF | sort -u >/etc/mkinitfs/features.d/virtio.modules.tmp
-$(cat /etc/mkinitfs/features.d/virtio.modules)
-kernel/drivers/char/hw_random/virtio-rng.ko
-EOF
-mv /etc/mkinitfs/features.d/virtio.modules.tmp /etc/mkinitfs/features.d/virtio.modules
+	cat <<-EOF | sort -u >/etc/mkinitfs/features.d/virtio.modules.tmp
+		$(cat /etc/mkinitfs/features.d/virtio.modules)
+		kernel/drivers/char/hw_random/virtio-rng.ko
+	EOF
+	mv /etc/mkinitfs/features.d/virtio.modules.tmp /etc/mkinitfs/features.d/virtio.modules
 
-# Make initramfs with features we think are spiffy
-# shellcheck disable=SC2016
-echo 'features="base ext2 ext3 ext4 keymap network packetrepo squashfs virtio eclypsium"' >/etc/mkinitfs/mkinitfs.conf
-kver=$(basename /lib/modules/*)
-mkinitfs -l "$kver"
-mkinitfs -o /assets/initramfs-$FLAVOR "$kver"
+	# Make initramfs with features we think are spiffy
+	# shellcheck disable=SC2016
+	echo 'features="base ext2 ext3 ext4 keymap network packetrepo squashfs virtio eclypsium"' >/etc/mkinitfs/mkinitfs.conf
+	kver=$(basename /lib/modules/*)
+	mkinitfs -l "$kver"
+	mkinitfs -o /assets/initramfs "$kver"
+}
 
-cp /boot/vmlinuz-$FLAVOR /assets/vmlinuz-$FLAVOR
+build_modloop() {
+	mkdir -p modloop/
+	cp -a /lib/modules/ modloop/
+	cp -a /lib/firmware/ modloop/modules
+	mksquashfs modloop/ /assets/modloop -b 1048576 -comp xz -Xdict-size 100% -noappend
+}
 
-# Make a new modloop
-mkdir -p modloop/
-cp -a /lib/modules/ modloop/
-cp -a /lib/firmware/ modloop/modules
-mksquashfs modloop/ /assets/modloop-$FLAVOR -b 1048576 -comp xz -Xdict-size 100% -noappend
+build_vmlinuz() {
+	cp /boot/vmlinuz-$FLAVOR /assets/vmlinuz
+}
+
+case $1 in
+vmlinuz) build_vmlinuz ;;
+initramfs) build_initramfs ;;
+modloop) build_modloop ;;
+*) build_vmlinuz build initramfs build_modloop ;;
+esac
