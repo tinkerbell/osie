4d3a01b305fa88f557b27b0199be31d0f1180cf5
Sarah Funkhouser <sfunkhouser@equinix.com>
2021-05-12 13:56:47 -0400

    ENG-15046 - add new metadata IP to list of metdata_urls
    Signed-off-by: Sarah Funkhouser <sfunkhouser@equinix.com>

--- docker/scripts/osie.sh
+++ docker/scripts/osie.sh
@@ -357,7 +357,7 @@ EOF
 			  Ec2:
 			    timeout: 60
 			    max_wait: 120
-			    metadata_urls: [ 'https://metadata.packet.net' ]
+			    metadata_urls: [ 'http://metadata.packet.net', '147.75.207.1' ]
 			    dsmode: net
 			disable_root: 0
 			package_reboot_if_required: false
