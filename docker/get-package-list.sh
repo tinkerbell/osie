773ddefedb29f38dbf47cfcea61d94c04fe89792
Manuel Mendez <mmendez@equinix.com>
2021-06-14 17:48:33 +0000

    osie: Pin grub to pre-Boothole fix
    Do this until we get the chroot based install working to unblock production.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>
    Signed-off-by: Andy Holtzmann <aholtzmann@equinix.com>

--- docker/get-package-list.sh
+++ docker/get-package-list.sh
@@ -6,6 +6,33 @@ x86_64) grub_efi_arch=amd64 ;;
 *) echo "unknown arch" && exit 1 ;;
 esac
 
+# pin grub to last known (but buggy!) versions
+cat >/etc/apt/preferences <<-EOF
+	Package: grub2
+	Pin: version 2.02~beta2-36ubuntu3.27
+	Pin-Priority: 999
+
+	Package: grub2-common
+	Pin: version 2.02~beta2-36ubuntu3.27
+	Pin-Priority: 999
+
+	Package: grub-common
+	Pin: version 2.02~beta2-36ubuntu3.27
+	Pin-Priority: 999
+
+	Package: grub-efi-$grub_efi_arch
+	Pin: version 2.02~beta2-36ubuntu3.27
+	Pin-Priority: 999
+
+	Package: grub-efi-$grub_efi_arch-bin
+	Pin: version 2.02~beta2-36ubuntu3.27
+	Pin-Priority: 999
+
+	Package: grub-pc-bin
+	Pin: version 2.02~beta2-36ubuntu3.27
+	Pin-Priority: 999
+EOF
+
 # shellcheck disable=SC2206
 packages=(
 	binutils
